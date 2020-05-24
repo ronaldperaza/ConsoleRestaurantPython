a935805bb105fe952e7d544ab6c43d6c66f37f18
ronaldperaza <ronaldperaza460@gmail.com>
2020-05-23 22:20:44 -0400

    tkinte menu firts commit

---
+++ menu.py
@@ -0,0 +1,13 @@
+import sqlite3
+from tkinter import *
+
+root = Tk()
+root.title("Restaurant Liah - Menu")
+root.resizable(0,0)  #evitar modificar el tamaño
+root.config(bd=25, relief="sunken") #border y relieve
+
+
+
+
+
+root.mainloop()
